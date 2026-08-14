4cab6fe54f697df70ce4f56164c8640d87d68dc0
NicoleMumo <nicolemumo3@gmail.com>
2026-08-14 16:55:31 +0300

    feat: embed YouTube player

--- js/app.js
+++ js/app.js
@@ -180,4 +180,12 @@ function generateRoomCode() {
     
 });
  
- 
+ let player;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player("player", {
+        height: "390",
+        width: "640",
+        videoId: "M7lc1UVf-VE"
+    });
+}
